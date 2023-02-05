aaa6c9c2af8473daed9aa123e984c4649ef04d8f
Jay3shh <jayesh921@gmail.com>
2023-02-05 18:29:41 +0530

    Update recon_v1_aws.sh

--- recon_v1_aws.sh
+++ recon_v1_aws.sh
@@ -6,7 +6,7 @@ else
     for sub in $( cat $1); do
         #echo "Initiating for $sub at $now" 
         #echo "[+] Starting SubFinder" &
-        /go/bin/subfinder -d $sub -silent &
+        /go/bin/subfinder -t 30 -d $sub -silent &
         /go/bin/assetfinder --subs-only $sub &
         findomain --quiet -t $sub &
         wait;
