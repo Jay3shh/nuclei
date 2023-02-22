2fa34078bf3f3507bd8946b83a65553ed4745138
Jay3shh <jayesh921@gmail.com>
2023-02-23 03:29:24 +0530

    Create recon_dalfox1.sh

---
+++ recon_dalfox1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#timeout --preserve-status -k 5s 14m 
+
+/go/bin/dalfox file $1 &
+dalfox_pid=$(pgrep -x dalfox)
+
+while kill -0 $dalfox_pid > /dev/null 2>&1; do
+    sleep 1  # wait for 1 second
+
+    if kill -0 $dalfox_pid > /dev/null 2>&1; then
+        mem_usage=$(ps -o rss= -p $dalfox_pid)
+        if [[ $mem_usage -ge 300000 ]]; then
+            echo "Memory usage exceeded 400 MB. Killing dalfox process."
+            kill $dalfox_pid  # kill the dalfox process
+            break
+        fi
+    fi
+done
