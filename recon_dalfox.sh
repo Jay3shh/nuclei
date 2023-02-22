4afb888dcfab9a8fbb48d00f7779a99711d70e53
Jay3shh <jayesh921@gmail.com>
2023-02-23 02:20:30 +0530

    Update recon_dalfox.sh

--- recon_dalfox.sh
+++ recon_dalfox.sh
@@ -8,7 +8,7 @@ while kill -0 $dalfox_pid > /dev/null 2>&1; do
 
     if kill -0 $dalfox_pid > /dev/null 2>&1; then
         mem_usage=$(ps -o rss= -p $dalfox_pid)
-        if [[ $mem_usage -ge 200000 ]]; then
+        if [[ $mem_usage -ge 300000 ]]; then
             echo "Memory usage exceeded 400 MB. Killing dalfox process."
             kill $dalfox_pid  # kill the dalfox process
             break
