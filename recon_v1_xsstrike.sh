9cff8f699d4487e8825517c56037f6d5e50c43cd
Jay3shh <jayesh921@gmail.com>
2023-02-23 17:28:14 +0530

    Create recon_v1_xsstrike.sh

---
+++ recon_v1_xsstrike.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+for urls in $( cat $1); do
+url=$urls
+done;
+
+
+output=""
+echo $url
+while read line; do
+  output="$output$line\n"
+  if echo "$line" | grep -q "Payload:"; then
+   pkill -f "xsstrike"
+   echo $line
+    # Do something
+  fi
+    sleep 1  # wait for 1 second
+    xsstrike_pid=$(pgrep -a "python" | grep "xsstrike.py" | grep -v "timeout" | awk '{print $1}')
+    if kill -0 $xsstrike_pid > /dev/null 2>&1; then
+        mem_usage=$(ps -o rss= -p $xsstrike_pid)
+        if [[ $mem_usage -ge 200000 ]]; then
+            echo "Memory usage exceeded 200 MB. Killing XSStrike process."
+            kill $xsstrike_pid  # kill the XSStrike process
+            break
+        fi
+    fi
+done < <(timeout --preserve-status -k 5s 5m python XSStrike/xsstrike.py -u "$url" --skip)
